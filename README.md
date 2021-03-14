dc00773f5c3d47936b9bb75c6f6569dca67002aa
chadrick-kwag <chadrick.kwag@gmail.com>
2021-03-14 14:07:18 +0900

    fix out ch bug

--- README.md
+++ README.md
@@ -19,6 +19,29 @@ $ python train_v1.py path/to/config
 
 the outputs will be saved in an automatically created directory under `train/ckpt/train_v1`
 
+## Slice Inference
+
+to run inference on a slice image, setup config file from sample in `predict/config/predict_config/sample.yaml` and run with the following command
+
+```
+$ cd predict
+$ python predict.py /config/file/path
+```
+
+this will save output mask images.
+
+## Slice Evaluate
+
+to run evaluation on slice image, setup config file from sample in `predict/config/eval_config/sample.yaml` and run with the following command
+
+```
+$ cd predcit
+$ python eval.py /config/file/path
+```
+
+this will save outputs including overlayed images and save dice values.
+
+
 ## Data preparation
 
 data preparation scripts are under `data_prep/`. these scripts are mainly slicing raw data, splitting, restructuring dataset structure, etc.
--- model/v2.py
+++ model/v2.py
@@ -1,23 +1,6 @@
 from efficientunet import get_efficientunet_b7, get_efficientunet_b0, get_efficientunet_b1, get_efficientunet_b2
 import torch
 
-# b0unet = get_efficientunet_b0(out_channels=1, concat_input=True, pretrained=True)
-
-
-# class EfficientUnetb7(torch.nn.Module):
-
-#     def __init__(self, in_ch):
-
-#         super().__init__()
-
-#         self.base = get_efficientunet_b7(out_channels=in_ch, concat_input=True, pretrained=True)
-
-#     def forward(self, x):
-#         y = self.base(x)
-#         y = y.permute(0,2,3,1)
-
-#         return y
-
 
 
 class EfficientUnetb0(torch.nn.Module):
@@ -26,7 +9,7 @@ class EfficientUnetb0(torch.nn.Module):
 
         super().__init__()
 
-        self.base = get_efficientunet_b0(out_channels=in_ch, concat_input=True, pretrained=True)
+        self.base = get_efficientunet_b0(out_channels=1, concat_input=True, pretrained=True)
 
     def forward(self, x):
         y = self.base(x)
@@ -42,7 +25,7 @@ class EfficientUnetb1(torch.nn.Module):
 
         super().__init__()
 
-        self.base = get_efficientunet_b1(out_channels=in_ch, concat_input=True, pretrained=True)
+        self.base = get_efficientunet_b1(out_channels=1, concat_input=True, pretrained=True)
 
     def forward(self, x):
         y = self.base(x)
@@ -58,7 +41,7 @@ class EfficientUnetb2(torch.nn.Module):
 
         super().__init__()
 
-        self.base = get_efficientunet_b2(out_channels=in_ch, concat_input=True, pretrained=True)
+        self.base = get_efficientunet_b2(out_channels=1, concat_input=True, pretrained=True)
 
     def forward(self, x):
         y = self.base(x)
